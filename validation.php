0f3c99a6c3814fe95a0fb758b6fccc0bbe45cf96
shubhamvir <47687080+shubhamvir@users.noreply.github.com>
2025-12-07 12:17:43 +0000

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,5 +1,5 @@
 <?php
-// validation.php - Input validation functions
+// validation.php
 
 function validate_username($username) {
     return preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username);
@@ -10,17 +10,10 @@ function validate_password($password) {
     return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/', $password);
 }
 
-function validate_vehicle_number($number) {
-    // Format: MP 04 1234 or similar
-    return preg_match('/^[A-Z]{2} \d{2} \d{4}$/', $number);
-}
-
-function validate_date($date) {
-    return (bool)strtotime($date);
-}
-
-function validate_number($number, $min = 0, $max = 999999) {
-    return filter_var($number, FILTER_VALIDATE_INT, 
-        array('options' => array('min_range' => $min, 'max_range' => $max)));
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+    return $data;
 }
 ?>
